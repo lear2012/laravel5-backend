a78001edf688bd58821eee4fde40156b8b7590d3
Lilin.Liao <lilin.liao@ecommerceclouds.com>
2017-04-07 20:32:28 +0800

    bug fixed

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
         '/wechat',
-        '/wechat/notify'
+        '/wechat/notify',
+        '/wechat/upload',
     ];
 }
