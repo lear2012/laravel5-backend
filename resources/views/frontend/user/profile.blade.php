82472df5ef6b5339a00d82998ff7547dabf51363
Lilin.Liao <lilin.liao@ecommerceclouds.com>
2017-04-13 21:53:39 +0800

    bug fixed

--- resources/views/frontend/user/profile.blade.php
+++ resources/views/frontend/user/profile.blade.php
@@ -40,6 +40,7 @@
             </p>
             @endif
         </div>
+        {{var_dump($carImgs);}}
         @if(count($carImgs) > 0)
         <div class="swiper-container" id="car_img_list">
             <div class="swiper-wrapper">
