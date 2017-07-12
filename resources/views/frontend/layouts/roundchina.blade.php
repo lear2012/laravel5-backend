f917305a01f0e47207421b01e3dbc54b2976f66e
Lilin.Liao <lilin.liao@ecommerceclouds.com>
2017-07-12 16:52:33 +0800

    bug fixed

--- resources/views/frontend/layouts/roundchina.blade.php
+++ resources/views/frontend/layouts/roundchina.blade.php
@@ -34,7 +34,7 @@
 @endif
 @if(isset($js))
     <script type="text/javascript" charset="utf-8">
-        wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage','onMenuShareQQ', 'onMenuShareWeibo'), ture); ?>);
+        wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage','onMenuShareQQ', 'onMenuShareWeibo'), true); ?>);
         wx.ready(function() {
             // 分享给朋友
             wx.onMenuShareAppMessage({
