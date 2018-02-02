7cddb3a1b572c300a2393051759bc2a8266c5356
liaolliso <liaolliso@gmail.com>
2018-02-02 16:22:50 +0800

    bug fixed

--- resources/views/frontend/xinjiang/xinzvideo.blade.php
+++ resources/views/frontend/xinjiang/xinzvideo.blade.php
@@ -7,11 +7,99 @@
         <div class="logo">
             <img src="img/keye.png" alt="可野">
         </div>
-        <div class="wenzi">
-            <img src="img/wenzi_xz.png" alt="新藏之旅">
-        </div>
     </header>
     <div class="content">
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/2.png">
+                <source src="video/2.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>绝不辜负那些在路上的美好时光</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/3.png">
+                <source src="video/3.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>欧加拉，微笑是世界上最美的语言</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/4.png">
+                <source src="video/4.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>可野环中国新疆段“食色之旅”维吾尔族美女跳舞</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/5.png">
+                <source src="video/5.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>惊！可野环中国车队竟然和新疆美女做这个！</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/6.png">
+                <source src="video/6.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>可野环中国新疆段——在巴里坤行车的旅人</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/7.png">
+                <source src="video/7.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>她来新疆二十年，竟在边境线开了一个深夜食堂</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/8.png">
+                <source src="video/8.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>梵高先生不在家</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/9.png">
+                <source src="video/9.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>离开公路 翻越山丘</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/10.png">
+                <source src="video/10.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>喀纳斯之夜，遇见灵魂歌手</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/11.png">
+                <source src="video/11.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>只要你霓虹闪烁，只要你在路上</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/12.png">
+                <source src="video/12.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>这世界上唯一的路</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/13.png">
+                <source src="video/13.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>塔城 晚风中的一棵树</p>
+        </div>
+        <div class="wrapper">
+            <video class="video" preload="none" poster="images/xinjiang/14.png">
+                <source src="video/14.mp4" type="video/mp4">
+            </video>
+            <div class="playpause"></div>
+            <p>喀什 我们不曾醒来 也不曾遗忘</p>
+        </div>
         <div class="wrapper">
             <video class="video" preload="none" poster="images/xinzang/1.png">
                 <source src="video/101.mp4" type="video/mp4">
@@ -39,6 +127,9 @@
             </video>
             <div class="playpause"></div>
             <p>惊！可野环中国车队竟然和新疆美女做这个！</p>
+        </div>
+        <div class="wrapper">
+            <embed src="https://imgcache.qq.com/tencentvideo_v1/playerv3/TPout.swf?max_age=86400&v=20161117&vid=y0541174hb7&auto=0" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
         </div>
 
         <footer>
